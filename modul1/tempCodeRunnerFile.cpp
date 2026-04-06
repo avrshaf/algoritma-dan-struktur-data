6592f4071622b7c58683aa662537e0606e7071f1
avrshaf <aurorashafasalsabilaa07@gmail.com>
2026-04-06 18:38:44 +0700

    Add files via upload

---
+++ modul1/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+
+                if(bayar[i].program<1 || bayar[i].program>2)
+                {
+                    cout<<"Program tidak sesuai\n";
+                    goto input;
+                } cout<<"Jumlah sks  = "; cin>>bayar[i].sks;
