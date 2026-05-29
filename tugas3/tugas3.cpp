f359fa8b20146ac9add21e1e0a2c088600b6d427
avrshaf <aurorashafasalsabilaa07@gmail.com>
2026-05-29 14:53:15 +0700

    revisi (part 2)

--- tugas3/tugas3.cpp
+++ tugas3/tugas3.cpp
@@ -14,56 +14,64 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-int binarySearch(int arr[], int kiri, int kanan, int target) {
+int binarySearchFirst(int arr[], int kiri, int kanan, int target) {
+    int hasil = -1;
     while (kiri <= kanan) {
         int tengah = kiri + (kanan - kiri) / 2;
-
         if (arr[tengah] == target) {
-            return tengah; 
-        } 
-        else if (arr[tengah] < target) {
-            kiri = tengah + 1; 
-        } 
-        else {
+            hasil = tengah;
             kanan = tengah - 1; 
+        } else if (arr[tengah] < target) {
+            kiri = tengah + 1;
+        } else {
+            kanan = tengah - 1;
         }
     }
-    return -1; 
+    return hasil;
 }
 
 int main() {
     int n, x;
-    
-    cin >> n >> x;
-    
-    int *arr = new int[n]; 
+    if (!(cin >> n >> x)) return 0;
+
+    int *arr = new int[n];
+    bool *used = new bool[n]; 
     
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
+        used[i] = false; 
     }
-    
+
     bubbleSort(arr, n);
-    
+
     int jumlahPasangan = 0;
-    
+
     for (int i = 0; i < n - 1; i++) {
-        
-        if (i > 0 && arr[i] == arr[i - 1]) {
+        if (used[i]) {
             continue;
         }
-        
+
         int angkaDicari = x - arr[i];
-        
-        int indeksKetemu = binarySearch(arr, i + 1, n - 1, angkaDicari);
-        
+
+        int indeksKetemu = binarySearchFirst(arr, i + 1, n - 1, angkaDicari);
+
         if (indeksKetemu != -1) {
-            jumlahPasangan++;
+            while (indeksKetemu < n && arr[indeksKetemu] == angkaDicari) {
+                if (!used[indeksKetemu]) {
+                    used[i] = true; 
+                    used[indeksKetemu] = true; 
+                    jumlahPasangan++;
+                    break; 
+                }
+                indeksKetemu++;
+            }
         }
     }
-    
+
     cout << jumlahPasangan << endl;
-    
+
     delete[] arr;
-    
+    delete[] used;
+
     return 0;
 }
